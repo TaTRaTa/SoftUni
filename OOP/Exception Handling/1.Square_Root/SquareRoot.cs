0eaeccc840da0a01688105c766555e81beb9d168
TaTRaTa <cvetomir.defyy@gmail.com>
2015-11-23 16:57:27 +0200

    Exception Handling

---
+++ OOP/Exception Handling/1.Square_Root/SquareRoot.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.Square_Root
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            //int input = ReadInteger();
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                 else if (!input.Equals(@"\\d+"))
+                {
+                    throw new FormatException();
+                }
+                 else
+                {
+                    Console.WriteLine(Math.Sqrt((double)input));
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+
+                if ((ex is ArgumentOutOfRangeException) )
+                {
+                    Console.WriteLine("Invalid number (ArgumentOutOfRangeException)");
+                }
+                else if (ex is FormatException)
+                {
+                    Console.WriteLine("Invalid number (FormatException)");
+                }
+                
+                
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+
+        //private static int ReadInteger()
+        //{
+        //    var input = Console.ReadLine();
+
+        //    try
+        //    {
+        //        return int.Parse(input);
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        Console.WriteLine("Please enter integer:");
+        //        return ReadInteger();
+        //    }
+        //}
+    }
+}
---
+++ OOP/Exception Handling/2.Enter_Numbers/EnterNumbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+namespace _2.Enter_Numbers
+{
+    class EnterNumbers
+    {
+        public static List<int> list = new List<int>(); 
+        private static void Main()
+        {
+            
+            const int START = 1;
+            const int END = 100;
+            for (int i = 0; i < 10; i++)
+            {
+                string input = Console.ReadLine();
+                ReadNumber(input, START, END);
+
+
+            }
+            
+
+        }
+
+        private static void ReadNumber(string input, int start, int end)
+        {
+            
+            int num;
+            bool isTrue = int.TryParse(input, out num);
+            list.Add(num);
+            if (list.Count - 1 != null)
+            {
+                try
+                {
+                    if (isTrue)
+                    {
+                        if (num < start || num > end || list.Count - 1 >= num)
+                        {
+                            throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("non-number");
+                    }
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("invalid number");
+                    ;
+                }
+                
+            }
+            
+            
+
+        }
+    }
+}
