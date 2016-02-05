9c5a3e95e935014524fcb25cd3c5fa83aa9c8420
TaTRaTa <cvetomir.defyy@gmail.com>
2016-02-05 18:14:17 +0200

    CSharp codes

---
+++ AdvancedCSharp/Exercises/AdvancedCSExam19July2015/03.RageQuit/RageQuit.cs
@@ -0,0 +1,32 @@
+﻿namespace _03.RageQuit
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    class RageQuit
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string pat = @"(.+?)(\d+)";
+            StringBuilder appndMatch = new StringBuilder();
+            Regex regex = new Regex(pat);
+            MatchCollection matches = regex.Matches(input.ToUpper());
+
+            foreach (Match match in matches)
+            {
+                int len = int.Parse(match.Groups[2].Value);
+                for (int i = 0; i < len; i++)
+                {
+                    appndMatch.Append(match.Groups[1].Value);
+                }
+            }
+
+            int count = appndMatch.ToString().Distinct().Count();
+            Console.WriteLine("Unique symbols used: {0}", count);
+            Console.WriteLine(appndMatch.ToString());        
+        }
+    }
+}
